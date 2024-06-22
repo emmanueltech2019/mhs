96c342328dbe9ab18572eeac4f8bc896801cf54d
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-06-22 16:13:04 +0100

    first commit

---
+++ src/app/data.tsx
@@ -0,0 +1,19 @@
+interface Data {
+    city: string;
+    state: string;
+    brand: string;
+    domain: string;
+    logo: string;
+    src: string;
+}
+const data:Data ={
+    brand:"MHS",
+    city:"Miami",
+    state:"FL",
+    domain: "miamiherniaspecialists.com",
+    logo: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1719069163/11Asset_40_k7c9rn.png',
+    src:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114964.39567715992!2d-80.31186053389821!3d25.782538873864524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9b0a20ec8c111%3A0xff96f271ddad4f65!2sMiami%2C%20FL%2C%20USA!5e0!3m2!1sen!2sng!4v1719069100914!5m2!1sen!2sng"
+
+}
+
+export default data
